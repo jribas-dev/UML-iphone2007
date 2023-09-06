0be1edc2056fa38e9b6ebb3c1ffabf0bdd0c9d99
jribas-dev <3b3brazil@gmail.com>
2023-09-06 15:00:31 -0300

    Main Class, init iphone2007, call methods

--- src/main/java/br/org/Main.java
+++ src/main/java/br/org/Main.java
@@ -1,7 +1,24 @@
 package br.org;
 
+import br.org.cellphone.CellPhone;
+import br.org.iphone2007.IPhone2007;
+import br.org.midiaplayer.MidiaPlayer;
+import br.org.webbrowser.WebBrowser;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        IPhone2007 iphone = new IPhone2007();
+
+        ((CellPhone) iphone).call("+5516992135544");
+        ((CellPhone) iphone).answer();
+        ((CellPhone) iphone).voiceMessage();
+
+        ((MidiaPlayer) iphone).select();
+        ((MidiaPlayer) iphone).play();
+        ((MidiaPlayer) iphone).pause();
+
+        ((WebBrowser) iphone).newProcess();
+        ((WebBrowser) iphone).showPage();
+        ((WebBrowser) iphone).refreshPage();
     }
 }
